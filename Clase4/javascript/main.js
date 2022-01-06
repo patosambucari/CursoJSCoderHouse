8e810f23612d0699e7e34d671f779ca5ee3de6f3
patosambucari <patosambucari@gmail.com>
2022-01-06 20:14:07 -0300

    Previa Clase 4 - 20220106 20:14

---
+++ Clase4/javascript/main.js
@@ -0,0 +1,13 @@
+h1 {
+    font:bold;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color:brown
+}
+
+h2 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+body {
+    background-color: burlywood;
+}
