afae658ab7b84e2e5bc74f15a7a682c545d6adbc
patosambucari <patosambucari@gmail.com>
2022-01-06 22:12:18 -0300

    Fin Clase 4 - 20220106 22:12

--- Clase4/javascript/main.js
+++ Clase4/javascript/main.js
@@ -1,13 +1,59 @@
-h1 {
-    font:bold;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    color:brown
+/*function sumar (a, b) {
+    return Number(a) + Number (b);
 }
 
-h2 {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+function duplicar (a) {
+    return (Number(a)*2);
 }
 
-body {
-    background-color: burlywood;
+let numero1 = prompt("Ingrese un numero");
+let numero2 = prompt("Ingrese otro numero");
+let resultado1 = sumar(numero1,numero2);
+let resultado2 = duplicar(numero1);
+let resultado3 = duplicar(numero2);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+
+alert("El resultado es " + resultado1);
+alert ("El doble del primer numero ingresado es " + resultado2);
+alert ("El doble del segundo numero ingresado es " + resultado3);
+
+
+//CALCULAR CUADRADO
+let num = prompt("Ingrese numero");
+
+function cuadrado (n){
+    return n*n;
 }
+
+alert(cuadrado(num)); */
+
+/*
+//FUNCION RECURSIVA
+
+//Bienvenida
+alert("¡CALCULO DEL FACTORIAL!")
+
+//Pido Ingreso de Numero
+numero = prompt ("Ingrese un numero para calcular su factorial");
+
+//Defino función recursiva
+const factorial = (n) => {
+    if (n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+
+//Muestro el resultado llamando a la función
+alert(`El factorial del numero ${numero} es ${factorial(numero)}`);*/
+
+let palabra = prompt("Ingresar Palabra");
+
+let palabraArray = palabra.split('');
+
+console.log (palabraArray[0]);
+//console.log (palabraArray[(palabraArray.length)]);
+console.log(palabraArray.length);
