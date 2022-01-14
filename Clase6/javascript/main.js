b3eb73ac941c4f3e1137213d57a467fae0375b94
patosambucari <patosambucari@gmail.com>
2022-01-13 22:34:29 -0300

    Final Clase 6 - 2022013 22:34

---
+++ Clase6/javascript/main.js
@@ -0,0 +1,19 @@
+const array1 = [1,2,3,4,5,6];
+
+for (i = 0 ; i < array1.length ; i++){
+    console.log(array1[i]);
+}
+
+array1.shift();
+
+for (i = 0 ; i < array1.length ; i++){
+    console.log(array1[i]);
+}
+
+console.log(array1);
+
+console.table(array1);
+
+
+console.log(typeof array1);
+//array1.forEach(Element => alert(Element));
